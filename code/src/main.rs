b72ffe95d3c46c1d17e6554d53c8aadb548a8fcb
Martin Kielhorn <kielhorn.martin@gmail.com>
2020-02-24 22:27:28 +0100

    initial commit

---
+++ code/src/main.rs
@@ -0,0 +1,218 @@
+extern crate core_affinity;
+use chrono::{DateTime, Utc};
+use crossbeam_channel::bounded;
+use glium::glutin;
+use glium::glutin::event::{Event, WindowEvent};
+use glium::glutin::event_loop::{ControlFlow, EventLoop};
+use glium::glutin::window::WindowBuilder;
+use glium::{Display, Surface};
+use imgui::*;
+use imgui::{Context, FontConfig, FontGlyphRanges, FontSource, Ui};
+use imgui_glium_renderer::Renderer;
+use imgui_winit_support::{HiDpiMode, WinitPlatform};
+use positioned_io::ReadAt;
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io;
+use std::sync::Mutex;
+use std::thread::spawn;
+use std::time::Instant;
+use std::{fs, thread, time};
+struct System {
+    event_loop: EventLoop<()>,
+    display: glium::Display,
+    imgui: Context,
+    platform: WinitPlatform,
+    renderer: Renderer,
+    font_size: f32,
+}
+fn init(title: &str) -> System {
+    let title = match title.rfind("/") {
+        Some(idx) => title.split_at((idx + 1)).1,
+        None => title,
+    };
+    let event_loop = EventLoop::new();
+    let context = glutin::ContextBuilder::new().with_vsync(true);
+    let builder = WindowBuilder::new()
+        .with_title(title.to_owned())
+        .with_inner_size(glutin::dpi::LogicalSize::new(512f64, 512f64));
+    let display =
+        Display::new(builder, context, &event_loop).expect("failed to initialize display");
+    let mut imgui = Context::create();
+    imgui.set_ini_filename(None);
+    let mut platform = WinitPlatform::init(&mut imgui);
+    {
+        let gl_window = display.gl_window();
+        let window = gl_window.window();
+        platform.attach_window(imgui.io_mut(), &window, HiDpiMode::Rounded);
+    };
+    let renderer = Renderer::init(&mut imgui, &display).expect("failed to initialize renderer");
+    return System {
+        event_loop,
+        display,
+        imgui,
+        platform,
+        renderer,
+        font_size: 12.,
+    };
+}
+impl System {
+    fn main_loop<F: FnMut(&mut bool, &mut Ui) + 'static>(self, mut run_ui: F) {
+        let System {
+            event_loop,
+            display,
+            mut imgui,
+            mut platform,
+            mut renderer,
+            ..
+        } = self;
+        let mut last_frame = Instant::now();
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::NewEvents(_) => {
+                last_frame = imgui.io_mut().update_delta_time(last_frame);
+            }
+            Event::MainEventsCleared => {
+                let gl_window = display.gl_window();
+                platform
+                    .prepare_frame(imgui.io_mut(), &(gl_window.window()))
+                    .expect("failed to prepare frame");
+                gl_window.window().request_redraw();
+            }
+            Event::RedrawRequested(_) => {
+                let mut ui = imgui.frame();
+                let mut run = true;
+                run_ui(&mut run, &mut ui);
+                if !(run) {
+                    *control_flow = ControlFlow::Exit;
+                };
+                let gl_window = display.gl_window();
+                let mut target = display.draw();
+                target.clear_color_srgb(1.0, 1.0, 1.0, 1.0);
+                platform.prepare_render(&ui, gl_window.window());
+                let draw_data = ui.render();
+                renderer
+                    .render(&mut target, draw_data)
+                    .expect("rendering failed");
+                target.finish().expect("swap buffer failed");
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+            event => {
+                let gl_window = display.gl_window();
+                platform.handle_event(imgui.io_mut(), gl_window.window(), &event);
+            }
+        });
+    }
+}
+fn main() {
+    let (s, r) = crossbeam_channel::bounded(4);
+    let history = std::sync::Arc::new(Mutex::new(VecDeque::with_capacity(100)));
+    {
+        let b = std::thread::Builder::new().name("deque_writer".into());
+        let history = history.clone();
+        b.spawn(move || {
+            loop {
+                let tup = r.recv().ok().unwrap();
+                let mut h = history.lock().unwrap();
+                h.push_back(tup);
+                if 20000 < h.len() {
+                    h.pop_front();
+                };
+            }
+        });
+    }
+    {
+        let core_ids = core_affinity::get_core_ids().unwrap();
+        for a in core_ids {
+            {
+                println!("{} {}:{} affinity  a={:?}", Utc::now(), file!(), line!(), a);
+            }
+        }
+        let b = std::thread::Builder::new().name("pluto_reader".into());
+        let reader_thread = b.spawn(move || {
+            core_affinity::set_for_current(core_affinity::CoreId { id: 0 });
+            loop {
+                s.send((Utc::now())).unwrap();
+            }
+        });
+    };
+    {
+        let system = init(file!());
+        let history = history.clone();
+        system.main_loop(move |_, ui| {
+            Window::new(im_str!("Hello world"))
+                .size([3.00e+2, 1.00e+2], Condition::FirstUseEver)
+                .build(ui, || {
+                    ui.text(im_str!("Hello World"));
+                    let mouse_pos = ui.io().mouse_pos;
+                    ui.text(format!("mouse: ({:.1},{:.1})", mouse_pos[0], mouse_pos[1]));
+                });
+            Window::new(im_str!("recv"))
+                .size([2.00e+2, 1.00e+2], Condition::FirstUseEver)
+                .build(ui, || {
+                    let h_guard = history.lock().unwrap();
+                    let h = h_guard.Deref();
+                    let mut time = vec![Utc::now(); h.len()];
+                    let mut data_time_between_samples_ms = vec![0.0f32; h.len()];
+                    let mut i = 0;
+                    for e in h {
+                        time[i] = e.0;
+                        if (0) == (i) {
+                            data_time_between_samples_ms[i] = 0.;
+                        } else {
+                            let duration = (time[i] - time[(i - 1)]);
+                            match duration.num_nanoseconds() {
+                                Some(a) => {
+                                    data_time_between_samples_ms[i] = ((1.00e-6) * (a as f32));
+                                }
+                                _ => {}
+                            };
+                        }
+                        i += 1;
+                    }
+                    {
+                        let mut mi = data_time_between_samples_ms[0];
+                        let mut ma = data_time_between_samples_ms[0];
+                        for e in &(data_time_between_samples_ms) {
+                            if *e < mi {
+                                mi = *e;
+                            };
+                            if ma < *e {
+                                ma = *e;
+                            };
+                        }
+                        let label =
+                            im_str!("time_between_samples_ms [-] {:12.4?} {:12.4?}", mi, ma);
+                        ui.plot_lines(
+                            &label,
+                            &(data_time_between_samples_ms[0..data_time_between_samples_ms.len()]),
+                        )
+                        .build();
+                    }
+                    {
+                        let mut mi = data_time_between_samples_ms[0];
+                        let mut ma = data_time_between_samples_ms[0];
+                        for e in &(data_time_between_samples_ms) {
+                            if *e < mi {
+                                mi = *e;
+                            };
+                            if ma < *e {
+                                ma = *e;
+                            };
+                        }
+                        let label =
+                            im_str!("time_between_samples_ms [-] {:12.4?} {:12.4?}", mi, ma);
+                        ui.plot_lines(
+                            &label,
+                            &(data_time_between_samples_ms[0..data_time_between_samples_ms.len()]),
+                        )
+                        .build();
+                    };
+                });
+        });
+    }
+}
---
+++ gen00.lisp
@@ -0,0 +1,401 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-rust-generator")
+  (ql:quickload "cl-ppcre"))
+
+(in-package :cl-rust-generator)
+
+(declaim (optimize (speed 0)
+		   (safety 3)
+		   (debug 3)))
+
+(progn
+  (defparameter *source-dir* #P"/home/martin/stage/cl-rust-adalm-pluto/code/")
+  (defparameter *code-file*
+    (merge-pathnames #P"src/main.rs"
+		     *source-dir*))
+  (defun logprint (msg &optional (rest nil))
+    `(progn
+       (println! (string ,(format nil "{} {}:{} ~a ~{~a~^ ~}"
+				  msg
+				  (loop for e in rest collect
+				       (format nil " ~a={:?}" (emit-rs :code e)))))
+		 #+nil (dot (SystemTime--now)
+		      (duration_since UNIX_EPOCH)
+		      (expect (string "time went backwards"))
+		      (as_micros))
+		 (Utc--now)
+		 (file!)
+		 (line!)
+		 ,@(loop for e in rest collect
+			e ;`(dot ,e (display))
+			))))
+
+
+  (with-open-file (s (merge-pathnames #P"Cargo.toml"
+				      *source-dir*)
+		     
+		     :direction :output
+		     :if-does-not-exist :create
+		     :if-exists :supersede)
+    (format s "~a"
+	    "[package]
+name = \"adalm_pluto_viewer\"
+version = \"0.1.0\"
+authors = [\"Martin Kielhorn <kielhorn.martin@gmail.com>\"]
+edition = \"2018\"
+
+# See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
+
+[dependencies]
+glium = \"*\"
+imgui = \"*\"
+imgui-glium-renderer = \"*\"
+imgui-winit-support = \"*\"
+chrono = \"*\"
+crossbeam-channel = \"*\"
+#positioned-io = \"*\"
+core_affinity = \"*\"
+industrial-io = \"*\" # 0.2.0
+
+# this shaves 1MB off the binary
+[profile.release]
+panic = \"abort\"
+"))
+
+  
+  
+  (let ((code
+	 `(do0
+	   (do0
+	    "extern crate core_affinity;"
+	    ;;"extern crate cpu_affinity;"
+	    (use (std thread spawn))
+	    (use (std io))
+	    (use
+	     (chrono (curly DateTime Utc)))
+	    "use imgui::*;"
+	    ,@(loop for e in `("glutin"
+			       "glutin::event::{Event,WindowEvent}"
+			       "glutin::event_loop::{ControlFlow,EventLoop}"
+			       "glutin::window::WindowBuilder"
+			       "{Display,Surface}")
+		 collect
+		   `(use (glium ,e))))
+	   (use (imgui (curly Context FontConfig FontGlyphRanges FontSource Ui))
+		(imgui_glium_renderer Renderer)
+		(imgui_winit_support (curly HiDpiMode WinitPlatform)))
+	   (use (std (curly thread time fs))
+		(std fs File)
+		(std time Instant)
+		(positioned_io ReadAt))
+
+	   (use (crossbeam_channel bounded)
+		(std collections VecDeque)
+		(std sync Mutex))
+	   
+
+	   ;(chrono::datetime::DateTime<chrono::offset::utc::Utc>, u64, u64, u64, u64, u64, u64, u64, u64, u64)
+	   
+	   (defstruct0 System
+	       (event_loop "EventLoop<()>")
+	     (display "glium::Display")
+	     (imgui Context)
+	     (platform WinitPlatform)
+	     (renderer Renderer)
+	     (font_size f32))
+
+
+	   (defun init ("title: &str")
+	     (declare (values System))
+	     (let ((title (case (title.rfind (string "/"))
+			    ((Some idx) (dot title (split_at (+ idx 1)) 1))
+			    (None title)))
+		   (event_loop ("EventLoop::new"))
+		   (context (dot ("glutin::ContextBuilder::new")
+				 (with_vsync true)))
+		   (builder (dot ("WindowBuilder::new")
+				 (with_title (title.to_owned))
+				 (with_inner_size
+				  ("glutin::dpi::LogicalSize::new"
+				   "512f64"
+				   "512f64"))))
+		   (display (dot ("Display::new"
+				  builder
+				  context
+				  &event_loop)
+				 (expect (string "failed to initialize display"))))
+		   (imgui ("Context::create")))
+	       (declare (mutable imgui))
+	       (imgui.set_ini_filename None)
+	       (let* ((platform ("WinitPlatform::init"
+				 "&mut imgui")))
+		 (progn
+		   (let ((gl_window (display.gl_window))
+			 (window (gl_window.window)))
+		     (platform.attach_window (imgui.io_mut)
+					     &window
+					     "HiDpiMode::Rounded"))))
+	      #+Nil(let ((hidpi_facotr (platform.hidpi_factor))
+		     (font_size (* 13s0 hidpi_factor)))
+		 (declare (type f32 font_size))
+		 (dot imgui
+		      (fonts)
+		      (add_font
+		       (ref (list
+			     (make-instance "FontSource::DefaultFontData"
+					    :config (Some
+						     (make-instance
+						      FontConfig
+						      :size_pixels font_size
+						      "" ("..FontConfig::default"))))
+			     (make-instance "FontSource::TtfData"
+					    ))))))
+	      (let ((renderer (dot ("Renderer::init"
+				    "&mut imgui"
+				    &display)
+				   (expect (string "failed to initialize renderer")))))
+		(return (make-instance System
+				       event_loop
+				       display
+				       imgui
+				       platform
+				       renderer
+				       :font_size 12s0)))))
+
+
+	   (impl System
+		 (defun "main_loop<F: FnMut(&mut bool,&mut Ui)+'static>"
+		     (self "mut run_ui: F")
+		   (let (((make-instance System
+					 event_loop
+					 display
+					 "mut imgui"
+					 "mut platform"
+					 "mut renderer"
+					 "..") self)
+			 (last_frame ("Instant::now")))
+		     (declare (mutable last_frame))
+		     (dot event_loop
+			  (run
+			   (space
+			    move
+			    (lambda (event _ control_flow)
+			      (case event
+				(("Event::NewEvents" _)
+				 (setf last_frame (dot imgui
+						       (io_mut)
+						       (update_delta_time last_frame)))
+				 )
+				("Event::MainEventsCleared"
+				 (let ((gl_window (display.gl_window)))
+				   (dot platform
+					(prepare_frame (imgui.io_mut)
+						       (ref (gl_window.window)))
+					(expect (string "failed to prepare frame")))
+				   (dot gl_window
+					(window)
+					(request_redraw))))
+				(("Event::RedrawRequested" _)
+				 (let* ((ui (imgui.frame))
+					(run true))
+				   (run_ui "&mut run"
+					   "&mut ui")
+				   (unless run
+				     (setf *control_flow
+					   "ControlFlow::Exit"))
+				   (let ((gl_window (display.gl_window)))
+				     (let* ((target (display.draw)))
+				       (target.clear_color_srgb 1s0 1s0 1s0 1s0)
+				       (platform.prepare_render &ui
+								(gl_window.window))
+				       (let ((draw_data (ui.render)))
+					 (dot renderer
+					      (render "&mut target"
+						      draw_data)
+					      (expect (string "rendering failed")))
+					 (dot target
+					      (finish)
+					      (expect (string "swap buffer failed"))))))))
+				((make-instance "Event::WindowEvent"
+						:event "WindowEvent::CloseRequested"
+						"..")
+				 (setf *control_flow "ControlFlow::Exit"))
+				(event
+				 (let ((gl_window (display.gl_window)))
+				   (platform.handle_event (imgui.io_mut)
+							  (gl_window.window)
+							  &event)))))))))))
+	  
+	   (defun main ()
+	     (let (((values s r) (crossbeam_channel--bounded 4))
+		   (history (std--sync--Arc--new (Mutex--new (VecDeque--with_capacity 100)))))
+	       (progn
+		 (let ((b (dot (std--thread--Builder--new)
+			       (name (dot (string "deque_writer")
+					  (into)))))
+		       (history (dot history (clone))))
+		   (b.spawn
+		    (space move
+			   (lambda ()
+			     (loop
+				(let ((tup (dot r
+						(recv)
+						(ok)
+						(unwrap))))
+				  (let* ((h (dot history
+						 (lock)
+						 (unwrap))))
+				    (dot
+				     h
+				     (push_back tup))
+				    (when (< 20000 (h.len))
+				      (h.pop_front))))))))))
+	       
+
+	       (progn
+		 (let ((core_ids (dot (core_affinity--get_core_ids)
+				      (unwrap))
+			 ))
+			   (for (a core_ids)
+				      ,(logprint "affinity" `(a))))
+		 (let (
+		       (b (dot (std--thread--Builder--new)
+			       (name (dot (string "pluto_reader")
+					  (into)))))
+		       (reader_thread
+			(b.spawn
+		    (space
+		     move
+		     (lambda ()
+		       (do0
+			(core_affinity--set_for_current (make-instance core_affinity--CoreId :id 0))
+
+			
+			(loop
+			   (dot s
+				(send
+				 (values (Utc--now)
+					 ))
+				(unwrap))))))))
+		       )
+		   		   
+		   
+		   
+		   )))
+	     #+nil (let* ((client (request--Client--new))
+			  (body (dot client
+				     (get (string "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=DBX,LITE,AMD,INTC&fields=regularMarketPrice"))
+				     (? await)
+				     (text)
+				     (? await))))
+		     
+		     ,(logprint "stock" `(body)))
+	     (progn
+	      (let ((system (init (file!)))
+		    (history (dot history (clone))))
+		(system.main_loop
+		 (space  move
+			 (lambda (_ ui)
+			   (dot ("Window::new" (im_str! (string "Hello world")))
+				(size (list 300.0 100.0) "Condition::FirstUseEver")
+				(build ui
+				       (lambda ()
+					 (ui.text (im_str! (string "Hello World")))
+					 (let ((mouse_pos (dot ui
+							       (io)
+							       mouse_pos)))
+					   (ui.text (format!
+						     (string "mouse: ({:.1},{:.1})")
+						     (aref mouse_pos 0)
+						     (aref mouse_pos 1)))))))
+			   (dot ("Window::new" (im_str! (string "recv")))
+				(size (list 200.0 100.0)
+				      "Condition::FirstUseEver")
+				;; https://github.com/Gekkio/imgui-rs/blob/master/imgui-examples/examples/test_window_impl.rs
+				(build ui
+				       (lambda ()
+					 (let ((h_guard (dot history (lock) (unwrap)))
+					       (h (dot h_guard
+						       (Deref)
+						       ;(iter)
+						       )))
+					   (let* ((time "vec![Utc::now();h.len()]")
+						  (data_time_between_samples_ms "vec![0.0f32;h.len()]")
+						  )
+
+					     (let* ((i 0))
+					       (for (e h)
+						    (setf (aref time i) e.0)
+						    (if (== 0 i)
+							(setf (aref data_time_between_samples_ms i) 0s0)
+							(let ((duration #+nil
+								(dot (aref time i)
+									     (signed_duration_since
+									      (aref time (- i 1))))
+								(- (aref time i)
+								   (aref time (- i 1)))))
+							  ;; (as_fractional_millis)
+							  ;; (as_seconds)
+							  (case (dot
+								   duration
+								   (num_nanoseconds))
+							    ((Some a)
+							     (setf (aref data_time_between_samples_ms i)
+								   (* 1e-6 (coerce
+								     a
+								     f32))
+								   ))
+							    (t )))) 
+						    
+						(incf i)))
+					     
+					     ,@(loop for timescale in `(1000 8000) appending
+						    (loop for (name f) in `(("time_between_samples_ms" "_"))
+						       collect
+							 (let ((dat (format nil "data_~a" name)))
+							   `(progn
+							      (let* ((mi (aref ,dat 0))
+								     (ma (aref ,dat 0)))
+								(for (e (ref ,dat))
+								     (when (< *e mi)
+								       (setf mi *e))
+								     (when (< ma *e)
+								       (setf ma *e)))
+								(let ((label (im_str! (string ,(format nil "~a [~a] {:12.4?} {:12.4?}"
+												       name
+												       (cond
+													 ((member name `(SVI2_C_Core
+															 SVI2_C_SoC
+															 ))
+													  "A")
+													 ((member name `(SVI2_Core
+															 SVI2_SoC
+															 ))
+													  "V")
+													 ((member name `(SVI2_C_Core
+															 SVI2_C_SoC
+															 SVI2_Core
+															 SVI2_SoC
+															 Tdie
+															 Tctl
+															 Tccd1))
+													  "degC")
+													 ((member name `(SVI2_P_Core
+															 SVI2_P_SoC))
+													  "W")
+													 (t "-"))))
+										      mi ma)))
+								  (dot ui (plot_lines
+									   &label
+									   (ref (aref ,dat
+										      (slice
+										       ;;std--cmp--max 0
+										       0 ;(- (dot ,dat (len)) ,timescale)
+										       
+										       (dot ,dat (len))))))
+											    (build))))))))))))))))))))))
+
+    
+    
+    (write-source *code-file*
+		  code)))
